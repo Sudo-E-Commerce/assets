374ffd0b73aa201e016d8b68f3ea8f6dbcd435e5
Nguyễn Viết Hiếu <36782331+hieunvph04479@users.noreply.github.com>
2020-05-09 11:59:56 +0700

    cập nhật thêm file và build html

--- config/SudoAsset.php
+++ config/SudoAsset.php
@@ -1,5 +1,68 @@
-<?php 
-
+<?php
 return [
+    // Mặc định sẽ là offline, assets sẽ được load từ local, nếu set offline là false và resource có định
+    // nghĩa cdn thì assets sẽ được load từ cdn
+    'offline' => env('ASSETS_OFFLINE', true),
 
+    // Bật hiển thị version, lúc này link tới resource sẽ được nối thêm "?v=1.0" chẳng hạn.
+    'enable_version' => true,
+
+    // Các thư viện js mặc định được sử dụng, là key được định nghĩa trong phần resource bên dưới.
+    'scripts' => [ 
+        'styles',
+    ],
+
+    // Các thư viện css mặc định
+    'styles' => [
+        'functions',
+        'main',
+    ],
+
+    // Định nghĩa tất cả đường dẫn tới assets.
+    'resources' => [
+    	// Định nghĩa các thư viện css
+    	'styles' => [ 
+            'style' => [
+            	// Có cho phép sử dụng cdn hay không, nếu là true thì bạn phải định nghĩa link tới cnd bên dưới
+	            'use_cdn' => false, 
+	            // Vị trí chèn, trên header hay dưới footer [top | bottom]
+	            'location' => 'bottom',
+	            'src' => [
+	            	// Đường dẫn tới thư viện
+	            	'local' => asset('/assets/css/style.min.css'),
+	            	'cdn' => null,
+	            ],
+	            // Các thuộc tính bổ sung, nếu cần
+	            'attributes' => [],
+	        ],
+        ],
+
+        // Định nghĩa các thư viện js
+        'scripts' => [
+            'main' => [
+            	// Có cho phép sử dụng cdn hay không, nếu là true thì bạn phải định nghĩa link tới cnd bên dưới
+	            'use_cdn' => false, 
+	            // Vị trí chèn, trên header hay dưới footer [top | bottom]
+	            'location' => 'bottom',
+	            'src' => [
+	            	// Đường dẫn tới thư viện
+	            	'local' => asset('/assets/js/main.min.js'),
+	            	'cdn' => null,
+	            ],
+	            // Các thuộc tính bổ sung, nếu cần
+	            'attributes' => [
+	            	'defer' => null,
+	            ],
+	        ],
+	        'functions' => [
+	        	'use_cdn' => false,
+	        	'location' => 'top',
+	        	'src' => [
+	            	'local' => asset('/assets/js/functions.min.js'),
+	            	'cdn' => null,
+	            ],
+	            'attributes' => [],
+	        ],
+        ],
+    ],
 ];
--- src/Facades/Asset.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Sudo\Asset\Facades;
-
-class Asset {
-
-	
-	
-}
